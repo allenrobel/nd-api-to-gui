274cebe7b233a768d9db1be0b72dc085f0948d70
Allen Robel <arobel@cisco.com>
2025-11-01 09:01:31 -1000

    __init__.py: expose more classes
    While not needed for the current scripts, let’s expose EpTemplate, EpTemplates, and ControllerResponseError as public classes in case folks want to import these for their own use.

--- nd_api_to_gui/__init__.py
+++ nd_api_to_gui/__init__.py
@@ -4,6 +4,8 @@ Nexus Dashboard API to GUI Package
 A Python package for mapping Nexus Dashboard API keys to GUI field names.
 """
 
+from nd_api_to_gui.ep_templates import EpTemplate, EpTemplates
+from nd_api_to_gui.exceptions import ControllerResponseError
 from nd_api_to_gui.log_v2 import Log
 from nd_api_to_gui.response_handler import ResponseHandler
 from nd_api_to_gui.rest_api_to_gui import RestApiToGui
@@ -12,12 +14,15 @@ from nd_api_to_gui.results_v2 import Results
 from nd_api_to_gui.sender_requests import Sender
 
 __all__ = [
+    "ControllerResponseError",
+    "EpTemplate",
+    "EpTemplates",
     "Log",
     "ResponseHandler",
-    "Sender",
+    "RestApiToGui",
     "RestSend",
     "Results",
-    "RestApiToGui",
+    "Sender",
 ]
 
 __version__ = "1.0.0"
